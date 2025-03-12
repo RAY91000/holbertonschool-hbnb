c795db1ee9a445131a04c89c5bddcf24ac654a17
ray91000 <ray.dotropg@gmail.com>
2025-03-12 05:14:55 +0100

    adding password

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from .base_model import BaseModel
+from app import bcrypt
 
 class User(BaseModel):
-    def __init__(self, first_name, last_name, email, is_admin=False):
+    def __init__(self, first_name, last_name, email, password, is_admin=False):
         super().__init__()
         self.first_name = first_name
         self.last_name = last_name
@@ -10,3 +11,16 @@ class User(BaseModel):
         self.is_admin = is_admin
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        self.password = None
+        self.hash_password(password)
+
+    def hash_password(self, password):
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+        return bcrypt.check_password_hash(self.password, password)
+
+    def to_dict(self):
+        dictionary = super().to_dict()
+        dictionary.pop('password', None)
+        return dictionary

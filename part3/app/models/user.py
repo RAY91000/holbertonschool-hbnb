33f4e3ac1cc5d825f0e083a96326dbb79a5e4500
Hikarikedo <keriandouidi01@gmail.com>
2025-03-13 14:49:27 +0100

    added dependencies imports and if not statements + new class Admin(User) promotion and demoting defs

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 from .base_model import BaseModel
-from app import bcrypt
+from app.persistence.repository import inMemoryRepository as database
+from flask_bcrypt import bcrypt
+bcrypt = Bcrypt()
+import uuid
 
 class User(BaseModel):
     def __init__(self, first_name, last_name, email, password, is_admin=False):
@@ -8,11 +11,11 @@ class User(BaseModel):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-        self.is_admin = is_admin
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        self.password = None
         self.hash_password(password)
+        self.is_admin = is_admin
+        self.validate_user()
 
     def hash_password(self, password):
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
@@ -20,7 +23,32 @@ class User(BaseModel):
     def verify_password(self, password):
         return bcrypt.check_password_hash(self.password, password)
 
-    def to_dict(self):
-        dictionary = super().to_dict()
-        dictionary.pop('password', None)
-        return dictionary
+    def validate_user(self):
+        if not self.first_name
+            raise ValueError("First name is required")
+        if not self.last_name
+            raise ValueError("Last name is required")
+        if not self.email
+            raise ValueError("Email is required")
+        emailregex = r'^[a-zA-Z0-9.+-]+@[a-zA-Z0-9-]+.[a-zA-Z]{2,}$'
+        if not re.match(email_regex, self.email):
+            raise ValueError("Invalid email format")
+        if not self.password
+            raise ValueError("Password is required")
+
+class Admin(User):
+    def __promote(self, user_id):
+        if self.is_admin == True:
+            update_date = datetime.now()
+            if database.update(self, user_id, {"is_admin": True, "update_date": update_date}) is not None:
+                print("User promotion to Admin")
+                return True
+            return False
+
+    def __demote(self, user_id):
+        if self.is_admin == False:
+            update_date = datetime.now()
+            if database.update(self,user_id, {"is_admin": True, "update_date": update}) is not None:
+                print("User is no longer Admin")
+                return True
+            return False
